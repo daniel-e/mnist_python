2c281dee712b8f1e0ed41039165cf7e361927dad
daniel-e <git.daniele@gmail.com>
2017-10-31 23:46:30 +0100

    updatee

---
+++ README.md
@@ -0,0 +1,4 @@
+# pymnist
+
+pymnist is a module which loads the
+[MNIST database of handwritten digits](http://yann.lecun.com/exdb/mnist/).
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymnist',
+  packages = ['pymnist'],
+  version = '0.1',
+  description = 'A library to load MNIST database of handwritten digits.',
+  author = 'daniel-e',
+  author_email = 'git.daniele@gmail.com',
+  url = 'https://github.com/daniel-e/pymnist',
+  download_url = 'https://github.com/daniel-e/pymnist/archive/0.1.tar.gz',
+  keywords = ['mnist', 'machine learning'],
+  classifiers = [],
+)
