b2f19aa18e92fbe276bed440b0f1d5f33ef259ed
daniel-e <git.daniele@gmail.com>
2017-10-31 23:58:57 +0100

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'pymnist',
-  packages = ['pymnist'],
-  version = '0.1',
+  name = 'mnistdb',
+  packages = ['mnistdb'],
+  version = '0.1.2',
   description = 'A library to load MNIST database of handwritten digits.',
   author = 'daniel-e',
   author_email = 'git.daniele@gmail.com',
-  url = 'https://github.com/daniel-e/pymnist',
-  download_url = 'https://github.com/daniel-e/pymnist/archive/0.1.1.tar.gz',
+  url = 'https://github.com/daniel-e/mnistdb',
+  download_url = 'https://github.com/daniel-e/mnistdb/archive/0.1.2.tar.gz',
   keywords = ['mnist', 'machine learning'],
   classifiers = [],
 )
